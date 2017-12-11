b66d7270e0c69b28381778796ae8f0d0eb907341
Igor Sharov <IgorSharov@users.noreply.github.com>
2017-12-11 18:37:33 +0300

    Layout

---
+++ src/components/layouts/MainLayout.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Segment, Header, Container } from 'semantic-ui-react';
+
+const MainLayout = ({ children }) => (
+  <Container>
+    <Logo />
+    { children }
+    <Footer />
+  </Container>
+);
+
+MainLayout.propTypes = {
+  children: PropTypes.node
+};
+
+export default MainLayout;
+
+const Logo = () => (
+  <Segment>
+    <Header>React blog</Header>
+  </Segment>
+);
+
+
+const Footer = () => (
+  <Segment>
+    Powered by iSharov
+  </Segment>
+);
